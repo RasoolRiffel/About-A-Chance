ffc88405461edc95114a462858cccb740638435f
Your Name <rasooldavids26@gmail.com>
2018-09-06 09:48:14 +0200

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,10 @@
+// Add active class to the current button (highlight it)
+    var header = document.getElementById("myDIV");
+    var btns = header.getElementsByClassName("nav");
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function() {
+          var current = document.getElementsByClassName("active");
+          current[0].className = current[0].className.replace(" active", "");
+          this.className += " active";
+        });
+      }
